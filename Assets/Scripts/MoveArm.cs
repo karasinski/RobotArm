f48b65b8d17c07a979aa042e3743c865f9cb740f
John Karasinski <karasinski@gmail.com>
2017-02-21 13:10:38 -0800

    can use keyboard to rotate parts of the arm incorrectly

---
+++ Assets/Scripts/MoveArm.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveArm : MonoBehaviour
+{
+    private GameObject part;
+    public float moveScale;
+
+    private string[] parts = new string[] { "armA:SR-joint", "armA:SY-joint", "armA:SP-joint", "armA:EP-joint", "armA:WP-joint", "armA:WY-joint", "armA:WR-joint" };
+    private int partIndex;
+
+    // Use this for initialization
+    void Start()
+    {
+        partIndex = 0;
+        setPart();
+    }
+
+    void setPart()
+    {
+        part = GameObject.Find(parts[partIndex]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float input = Input.GetAxis("Horizontal");
+        part.transform.Rotate(moveScale * input * Time.deltaTime, 0, 0);
+
+        if (Input.GetKeyUp(KeyCode.UpArrow) && partIndex < parts.Length)
+        {
+            partIndex++;
+            setPart();
+        }
+        if (Input.GetKeyUp(KeyCode.DownArrow) && partIndex > 0)
+        {
+            partIndex--;
+            setPart();
+        }
+        print(partIndex);
+    }
+}
